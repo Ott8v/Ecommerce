70a0430a571b75e22a7ffc9f1d8deced79dbae7c
Federico Balducci <ott8v@proton.me>
2024-02-22 21:47:35 +0100

    resolved v-show showing

--- src/components/LoginDash.vue
+++ src/components/LoginDash.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="q-pa-md">
-    <q-btn color="black" :label="stringChange" @click="goLogin()">
-      <div v-show="doNotShow">
-        <q-menu>
-          <div class="row no-wrap q-pa-md">
-            <div class="column">
-              <div class="text-h6 q-mb-md">Settings</div>
-              <q-toggle v-model="mobileData" label="Use Mobile Data" />
-              <q-toggle v-model="bluetooth" label="Bluetooth" />
-            </div>
-
-            <q-separator vertical inset class="q-mx-lg" />
-
-            <div class="column items-center">
-              <q-avatar size="72px">
-                <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
-              </q-avatar>
-
-              <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
-
-              <q-btn
-                color="primary"
-                label="Logout"
-                push
-                size="sm"
-                @click="logOut()"
-                v-close-popup
-              />
-            </div>
+    <q-btn color="black" :label="stringChange">
+      <q-menu>
+        <div class="row no-wrap q-pa-md" v-show="doNotShow">
+          <div class="column">
+            <div class="text-h6 q-mb-md">Settings</div>
+            <q-toggle v-model="mobileData" label="Use Mobile Data" />
+            <q-toggle v-model="bluetooth" label="Bluetooth" />
           </div>
-        </q-menu>
-      </div>
+
+          <q-separator vertical inset class="q-mx-lg" />
+
+          <div class="column items-center">
+            <q-avatar size="72px">
+              <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
+            </q-avatar>
+
+            <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+            <q-btn
+              color="primary"
+              label="Logout"
+              push
+              size="sm"
+              @click="logOut()"
+              v-close-popup
+            />
+          </div>
+        </div>
+      </q-menu>
     </q-btn>
   </div>
 </template>
@@ -54,5 +52,8 @@ const goLogin = () => {
     route.push({ name: "login" });
   }
 };
-const logOut = store.logOut();
+
+const logOut = () => {
+  store.logOut();
+};
 </script>
