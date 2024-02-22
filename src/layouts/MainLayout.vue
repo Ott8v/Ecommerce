1d608dc879823d06aaade110a5074c815b968d96
Federico Balducci <ott8v@proton.me>
2024-02-22 18:31:33 +0100

    create loginDash component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,38 +12,7 @@
         />
 
         <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div class="q-pa-md">
-          <q-btn color="purple" label="Account Settings">
-            <q-menu>
-              <div class="row no-wrap q-pa-md">
-                <div class="column">
-                  <div class="text-h6 q-mb-md">Settings</div>
-                  <q-toggle v-model="mobileData" label="Use Mobile Data" />
-                  <q-toggle v-model="bluetooth" label="Bluetooth" />
-                </div>
-
-                <q-separator vertical inset class="q-mx-lg" />
-
-                <div class="column items-center">
-                  <q-avatar size="72px">
-                    <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
-                  </q-avatar>
-
-                  <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
-
-                  <q-btn
-                    color="primary"
-                    label="Logout"
-                    push
-                    size="sm"
-                    v-close-popup
-                  />
-                </div>
-              </div>
-            </q-menu>
-          </q-btn>
-        </div>
+        <LoginDash></LoginDash>
       </q-toolbar>
     </q-header>
 
@@ -68,6 +37,7 @@
 <script>
 import { defineComponent, ref } from "vue";
 import EssentialLink from "components/EssentialLink.vue";
+import LoginDash from "components/LoginDash.vue";
 
 const linksList = [
   {
@@ -119,6 +89,7 @@ export default defineComponent({
 
   components: {
     EssentialLink,
+    LoginDash,
   },
 
   setup() {
