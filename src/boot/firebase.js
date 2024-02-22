a96286bf2c31d69b419b4ec3d7b8ab2b0b9f8f8e
Federico Balducci <ott8v@proton.me>
2024-02-22 09:50:30 +0100

    Co-authored-by: Cattani Giacomo

---
+++ src/boot/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCVzfM7FNLv3lMrfrBCzWzK298FDmgeGs4",
+
+  authDomain: "ecommerce-6819d.firebaseapp.com",
+
+  projectId: "ecommerce-6819d",
+
+  storageBucket: "ecommerce-6819d.appspot.com",
+
+  messagingSenderId: "432754348678",
+
+  appId: "1:432754348678:web:a6619ad82fd2e5cf327707"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
