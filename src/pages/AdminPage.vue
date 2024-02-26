6985d8032a06f130e70d6ffefaf53cfed5a5cbba
Federico Balducci <ott8v@proton.me>
2024-02-26 17:35:31 +0100

    update

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-card flat bordered>
-    <q-card-section> Lorem ipsum dolor sit amet </q-card-section>
+    <q-card-section> Create Item </q-card-section>
     <q-card-actions align="right">
       <q-btn flat label="Create" />
     </q-card-actions>
