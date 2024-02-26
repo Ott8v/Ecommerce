a843524f6801e883188b9159368c332b3b8ff631
Federico Balducci <ott8v@proton.me>
2024-02-26 22:07:30 +0100

    update CreateDash component

--- src/components/CreateDash.vue
+++ src/components/CreateDash.vue
@@ -1,9 +1,38 @@
 <template>
-  <q-card flat bordered>
-    <q-card-section> Create Item </q-card-section>
-    <q-card-actions align="right">
-      <q-btn flat label="Create" />
-    </q-card-actions>
-  </q-card>
+  <div class="row">
+    <div class="column items-center">
+      <q-card flat bordered>
+        <q-card-section> Create Item </q-card-section>
+        <q-card-section>
+          <q-input outlined dense v-model="item.name" label="Name" />
+          <q-input
+            outlined
+            dense
+            type="number"
+            v-model="item.quantity"
+            label="Quantity"
+          />
+          <q-input
+            outlined
+            dense
+            type="number"
+            prefix="€"
+            v-model="item.price"
+            label="Price"
+          />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Create" />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+let item = ref({
+  name: undefined,
+  quantity: undefined,
+  price: undefined,
+});
+</script>
--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,6 +1,9 @@
 <template>
+  <h3>Hi {{ store.getInfo.nome }} {{ store.getInfo.cognome }},</h3>
   <CreateDash />
 </template>
 <script setup>
 import CreateDash from "../components/CreateDash.vue";
+import { userStore } from "../stores/user.js";
+const store = userStore();
 </script>
