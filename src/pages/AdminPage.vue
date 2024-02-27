0fabeeae739bcc907e798ca1ee8e0d7604afd0c2
Federico Balducci <ott8v@proton.me>
2024-02-27 21:54:31 +0100

    update admninPage interface

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row">
-    <div class="col">
+    <div class="col q-pl-lg">
       <h3>Hi {{ store.getInfo.nome }} {{ store.getInfo.cognome }},</h3>
       <p>
         Welcome to the admin page. Here you can create/delete and update items.
       </p>
     </div>
-    <div class="col">
+    <div class="col q-pt-xl">
       <CreateDash />
     </div>
   </div>
