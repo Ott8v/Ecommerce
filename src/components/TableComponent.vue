c4e948da10fdd2733a3acbbb177d70f260611263
Federico Balducci <ott8v@proton.me>
2024-02-29 11:14:30 +0100

    update AdminPage and table component

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -13,48 +13,5 @@
   </div>
 </template>
 <script setup>
-import { collection, getDocs, getFirestore } from "firebase/firestore";
-import { ref, onBeforeMount } from "vue";
-const db = getFirestore();
-const querySnapshot = ref(null);
-let rows = ref([]);
-
-async function getAllItems() {
-  querySnapshot.value = await getDocs(collection(db, "items"));
-  querySnapshot.value.forEach((doc) => {
-    let obj = {
-      id: doc.id,
-      name: doc.data().name,
-      quantity: doc.data().quantity,
-      price: doc.data().price,
-    };
-    rows.value.push(obj);
-  });
-}
-const columns = ref([
-  {
-    name: "id",
-    label: "Id",
-    field: "id",
-  },
-  {
-    name: "name",
-    label: "Name",
-    field: "name",
-  },
-  {
-    name: "quantity",
-    label: "Quantity",
-    field: "quantity",
-  },
-  {
-    name: "price",
-    label: "Price",
-    field: "price",
-  },
-]);
-
-onBeforeMount(() => {
-  getAllItems();
-});
+defineProps({ columns: Array, rows: Array });
 </script>
--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -11,7 +11,7 @@
       <CreateDash />
     </div>
     <div class="column">
-      <TableComponent />
+      <TableComponent :columns="columns" :rows="rows" />
     </div>
   </div>
 </template>
@@ -19,5 +19,49 @@
 import CreateDash from "../components/CreateDash.vue";
 import TableComponent from "../components/TableComponent.vue";
 import { userStore } from "../stores/user.js";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { ref, onBeforeMount } from "vue";
 const store = userStore();
+const db = getFirestore();
+const querySnapshot = ref(null);
+let rows = ref([]);
+
+async function getAllItems() {
+  querySnapshot.value = await getDocs(collection(db, "items"));
+  querySnapshot.value.forEach((doc) => {
+    let obj = {
+      id: doc.id,
+      name: doc.data().name,
+      quantity: doc.data().quantity,
+      price: doc.data().price,
+    };
+    rows.value.push(obj);
+  });
+}
+const columns = ref([
+  {
+    name: "id",
+    label: "Id",
+    field: "id",
+  },
+  {
+    name: "name",
+    label: "Name",
+    field: "name",
+  },
+  {
+    name: "quantity",
+    label: "Quantity",
+    field: "quantity",
+  },
+  {
+    name: "price",
+    label: "Price",
+    field: "price",
+  },
+]);
+
+onBeforeMount(() => {
+  getAllItems();
+});
 </script>
