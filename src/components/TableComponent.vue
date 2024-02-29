1bf48021136d3ce80259cb3815fc7bca61d9c923
Federico Balducci <ott8v@proton.me>
2024-02-29 10:41:33 +0100

    update TableComponent

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -4,14 +4,62 @@
       style="height: 400px"
       flat
       bordered
-      title="Treats"
+      title="Items"
       :rows="rows"
       :columns="columns"
-      row-key="index"
       virtual-scroll
-      v-model:pagination="pagination"
       :rows-per-page-options="[0]"
     />
   </div>
 </template>
-<script setup></script>
+<script setup>
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  snapshotEqual,
+} from "firebase/firestore";
+import { ref, onBeforeMount, computed } from "vue";
+const db = getFirestore();
+const querySnapshot = ref(null);
+let rows = ref([]);
+
+async function getAllItems() {
+  querySnapshot.value = await getDocs(collection(db, "items"));
+  querySnapshot.value.forEach((doc) => {
+    let obj = {
+      id: doc.id,
+      name: doc.data().name,
+      quantity: doc.data().quantity,
+      price: doc.data().price,
+    };
+    rows.value.push(obj);
+  });
+}
+const columns = ref([
+  {
+    name: "id",
+    label: "Id",
+    field: "id",
+  },
+  {
+    name: "name",
+    label: "Name",
+    field: "name",
+  },
+  {
+    name: "quantity",
+    label: "Quantity",
+    field: "quantity",
+  },
+  {
+    name: "price",
+    label: "Price",
+    field: "price",
+  },
+]);
+
+onBeforeMount(() => {
+  getAllItems();
+});
+</script>
