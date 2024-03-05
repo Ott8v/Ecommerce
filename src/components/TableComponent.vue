2bc85ce9d394d70af86744751bc739b73ea425db
Federico Balducci <ott8v@proton.me>
2024-03-05 18:01:47 +0100

    add posibility to add function on row click (TableComponent)

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -11,9 +11,16 @@
       separator="cell"
       :pagination="{ rowsPerPage: 0 }"
       :rows-per-page-options="[0]"
+      @row-click="(index) => clickRow(index)"
     />
   </div>
 </template>
 <script setup>
-defineProps({ title: String, columns: Array, rows: Array, style: Object });
+defineProps({
+  title: String,
+  columns: Array,
+  rows: Array,
+  style: Object,
+  clickRow: { type: Function, require: false, default: () => {} },
+});
 </script>
